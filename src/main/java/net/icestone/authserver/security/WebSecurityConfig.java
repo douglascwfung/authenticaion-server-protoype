ece67bbe27d103c971799cb6ed0c25433fa06e00
Douglas Fung <icestone.net@gmail.com>
2022-09-16 16:23:59 +0800

    add jwt token

--- src/main/java/net/icestone/authserver/security/WebSecurityConfig.java
+++ src/main/java/net/icestone/authserver/security/WebSecurityConfig.java
@@ -10,12 +10,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import net.icestone.authserver.filter.JWTTokenGeneratorFilter;
+import net.icestone.authserver.filter.JWTTokenValidatorFilter;
+import net.icestone.authserver.filter.RequestValidationBeforeFilter;
 import net.icestone.authserver.service.UserService;
 
 @EnableWebSecurity(debug = true)
@@ -41,30 +46,37 @@ public class WebSecurityConfig {
 //		authenticationManagerBuilder.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
 
 		http
-//		.cors().configurationSource(new CorsConfigurationSource() {
-//            @Override
-//            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-//                CorsConfiguration config = new CorsConfiguration();
-////                config.setAllowedOrigins(Arrays.asList("http://localhost"));
-////                config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
-//                config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:8080"));
-//                config.setAllowedMethods(Collections.singletonList("*"));
-//                config.setAllowCredentials(true);
-//                config.setAllowedHeaders(Collections.singletonList("*"));
-//                config.setMaxAge(3600L);
-//                return config;
-//            }
-//        })
-//		.and()
+		
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+//		.cors().disable()
+		.cors().configurationSource(new CorsConfigurationSource() {
+            @Override
+            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                CorsConfiguration config = new CorsConfiguration();
+//                config.setAllowedOrigins(Arrays.asList("http://localhost"));
+//                config.setAllowedOrigins(Collections.singletonList("http://localhost:8080"));
+                config.setAllowedOriginPatterns(Collections.singletonList("*"));
+                config.setAllowedMethods(Collections.singletonList("*"));
+                config.setAllowCredentials(true);
+                config.setAllowedHeaders(Collections.singletonList("*"));
+                config.setExposedHeaders(Arrays.asList("Authorization"));
+                config.setMaxAge(3600L);
+                return config;
+            }
+        })
+		.and()
 //		.csrf().disable()
 		.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 		.and()
+		.addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
+        .addFilterBefore(new JWTTokenValidatorFilter(), BasicAuthenticationFilter.class)
+        .addFilterAfter(new JWTTokenGeneratorFilter(), BasicAuthenticationFilter.class)		
 		.authenticationProvider(customPwdAuthenticationProvider)		
 		.authorizeHttpRequests( (auth)->auth				
 				.antMatchers("/api/test/auth").authenticated()
 				.antMatchers("/api/test/admin").hasRole("ADMIN")
 				.antMatchers("/api/test/post").hasRole("ADMIN")
-				.antMatchers("/api/test/user").hasRole("USER")
+				.antMatchers("/api/test/user").authenticated()
 //				.antMatchers("/api/test/user").hasAuthority(EAuthority.DELETE.toString())
 				//.antMatchers("/api/test/mod").hasRole("MODERATOR")
 				.antMatchers("/api/test/all").permitAll()
